fd168dbcd929393dad9396125e6c711c73485a24
Warawut <radchadokimidon@gmail.com>
2025-08-04 17:57:40 +0700

    treatmentzone

--- Assets/Scripts/PatientStatus.cs
+++ Assets/Scripts/PatientStatus.cs
@@ -12,11 +12,17 @@ public class PatientStatus : MonoBehaviour
 
     [Header("References")]
     public GameObject bloodEffect;
-    public Animator animator;
+
     public StretcherController assignedStretcher; // ลิงก์เปลที่รับผู้ป่วยคนนี้
 
     [Header("Timer")]
     public float timeCountdown = 0f;
+
+    [Header("Green Zone")]
+    public Transform greenZone; // กำหนดจาก Inspector
+    public float walkSpeed = 1.5f;
+
+    public Animator animator; // 1=เดิน, 2=นั่ง, , 3=ลุกขึ้น ,7=ตาย,
 
     void Update()
     {
@@ -24,6 +30,14 @@ public class PatientStatus : MonoBehaviour
         {
             case 1:
                 TimeCount();
+                break;
+
+            case 2:
+                if (triageColor == 1 && greenZone != null)
+                {
+                    MoveToGreenZone();
+                    
+                }
                 break;
 
             case 6:
@@ -48,6 +62,9 @@ public class PatientStatus : MonoBehaviour
                 BecomeBlack();
                 break;
         }
+
+        
+
     }
 
 
@@ -56,7 +73,7 @@ public class PatientStatus : MonoBehaviour
         timeCountdown += Time.deltaTime;
     }
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("TriageTag") && status != 6)
         {
@@ -65,6 +82,31 @@ public class PatientStatus : MonoBehaviour
             PlayerScore.score += 1;
         }
     }
+
+    void MoveToGreenZone()
+    {
+        Vector3 direction = (greenZone.position - transform.position).normalized;
+        animator.SetInteger("movement", 2);
+
+        // เดินไปตำแหน่ง greenZone
+        transform.position = Vector3.MoveTowards(transform.position, greenZone.position, walkSpeed * Time.deltaTime);
+
+        // หันหน้าไปทิศทางเดิน (ลบ Y เพราะไม่อยากให้เงย/ก้ม)
+        if (direction != Vector3.zero)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f); // 5f = ความเร็วหมุน
+        }
+
+        float distance = Vector3.Distance(transform.position, greenZone.position);
+        if (distance < 0.1f)
+        {
+            status = 10;
+            animator.SetInteger("movement", 1);
+            Debug.Log($"{gameObject.name} ถึงโซนสีเขียวแล้ว → status = 10");
+        }
+    }
+
 
     void BecomeBlack()
     {
